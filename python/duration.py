ca3f0538353ecc3d71dc2fa3031cf75d416383e4
EdoardoBonato <146087382+EdoardoBonato@users.noreply.github.com>
2024-03-19 23:40:02 +0100

    Update and rename duration1_adj_region.py to duration.py

--- python/duration.py
+++ python/duration.py
@@ -1,28 +1,29 @@
+#from the extended datasets obtain the grouped version
+#the extended version contains all observations, i.e. every year in which every guild is observed
+#the gruoped version will have as observation the number of guilds, with their year of born and death
+
 import PyPDF2
 import pandas as pd
 import numpy as np
 from collections import defaultdict
-from sentence_transformers import SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
-import math
 import sys
-import json
 sys.path.append(r"C:\Users\edobo\OneDrive\Desktop\Python\modules")
 from data_structures import list_to_dict
 from data_structures import merge_dict
 from statistical_calculations import frequencies
 from text_analysis import find_similarity
-import plotly.express as px
-import plotly.io as pio
-import plotly.graph_objects as go
 import random
 from text_analysis import find_similarity, compare_dict
-import random
 
+#import merged datasets
 path = r"C:\Users\edobo\OneDrive\Desktop\Thesis\Medieval Guilds\Data\EDITED_db\Merge\merged_w_abolition1_region.xlsx"
 merged = pd.read_excel(path)
+
+#keep only the information about the abolished guilds
 merged = merged[(merged.change == 'IS ABOLISHED') | (merged['change'].isna())]
+#clean from no sense values
 merged = merged[merged['combined_years'] != 0]
+
 grouped = merged.groupby(['place', 'guild_name'])
 grouped_min_max = grouped.agg({'region': 'first', 'combined_years' : ['min', 'max']})
 def approximate_year(year):
@@ -36,6 +37,6 @@ grouped_min_max['duration'] = grouped_min_max['max_year'] - grouped_min_max['min
 calculate_duration = grouped_min_max[grouped_min_max.duration > 20]
 average_duration = np.mean(calculate_duration['duration'])
 
-
+#export to dataset
 grouped_min_max = grouped_min_max.sort_values(by = 'duration', ascending = False)   
 grouped_min_max.to_excel(r'C:\Users\edobo\OneDrive\Desktop\Thesis\Medieval Guilds\Data\EDITED_db\Merge\grouped_min_max1_region.xlsx')
